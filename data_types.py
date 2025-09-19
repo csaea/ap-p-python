984b602115da4b7a9a27684193e8b4940ab18ed1
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-09-19 19:08:00 +0000

    quick list of datatypes

---
+++ data_types.py
@@ -0,0 +1,36 @@
+# The following are common (but not all) data types for python. 
+#These are the only ones you'll need to know for AP CSP exam
+
+#STRING 
+#Regular language, in ASCII characters including number, contained by quotations marks
+print("Hello world")
+print("-----")
+print("Once upon a time and a very good time it was there was a moocow coming down along the road and this moocow that was down along the road met a nicens little boy named baby tuckoo")
+print(" ")
+print("Once upon a time \nand a very good time it was \nthere was a moocow\n")
+
+#INTEGER 
+#whole numbers, positive and negative
+print(42)
+print(23)
+print(5 + 5)
+
+#FLOAT
+#decimal point numbers.
+print(4.0)
+print(2.0 + 4.5)
+print(10 / 5) # / division always returns a float. To return integer, use //
+
+#BOOLEAN
+print(True)
+print(False)
+print(1 == 1)
+print(1 != 1)
+print(5 + 5 == 11)
+print("Hello" == "Hello")
+
+#How do I know what data type I'm working with? 
+type("Hello world")
+type(3)
+type(3.14)
+type(True)
