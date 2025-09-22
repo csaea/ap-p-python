ea698a18b441f7d2a3adbdb607bde443653a78bf
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-09-22 13:26:51 +0000

    updates variables lesson

--- lesson02_variables.py
+++ lesson02_variables.py
@@ -1,1 +1,48 @@
+#CONCEPTS VARIABLES
+
+# use descriptive variables names. variable names can be overwritten
+
+password = "G00seberryPie5"
+email = "mharrell@nhvweb.net"
+
+count = 42
+temp = 3
+
+temperature = 72.5
+price = 3.99
+
+enabled = False
+is_complete = True
+
+print(is_complete)
+
+enabled = False
+#print(is_complete)
+
+# for loops and math problems, you will see single letters, but otherwise AVOID THIS PRACTICE!
+
+x = 3.14
+y = 7
+
+i = 0
+j = i + 1
+
+#CHALLENGES
+
+# Edit the variable names to be more meaningful
+
+x = "Radia Perlman"
+y = 34
+z = "Networking Engineer"
+
+#Given variables x = 4 and y = "hello". Swap the variables so that x = "hello" and y = 4. Hint: You will need to create one new variable. 
+
+x = 4
+y = "hello"
+temp = x
+x = y
+y = temp
+
+print("x = ", x)
+print("y = ", y)
 
