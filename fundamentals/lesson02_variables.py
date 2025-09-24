a64bd8e6ebdecdef194eff571d1782e7033e703b
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-09-24 14:56:41 +0000

    updates lesson 2 and 3

--- fundamentals/lesson02_variables.py
+++ fundamentals/lesson02_variables.py
@@ -33,13 +33,25 @@ print(x, y)
 
 #CHALLENGES
 
-# Edit the variable names to be more meaningful
+# Challenge 1: Rename Variables  
+# Change the variable names x, y, z below to more descriptive names. 
 
 x = "Radia Perlman"
 y = 34
 z = "Networking Engineer"
 
-#Given variables x = 4 and y = "hello". Swap the variables so that x = "hello" and y = 4. Hint: You will need to create one new variable. 
+# Challenge 2: Update Variables  
+# Create a variable called 'count' with a value of 10.  
+# Increase it by 5 and print the result.  
+
+count = 10
+adder = count + 5
+print(adder)
+
+# Challenge 3: Swap Variables  
+# Given variables x = 4 and y = "hello".  
+# Swap the values so that x = "hello" and y = 4. Use a temporary variable.  
+# Hint: You will need to create one new variable. 
 
 x = 4
 y = "hello"
@@ -50,3 +62,11 @@ y = temp
 print("x = ", x)
 print("y = ", y)
 
+# Challenge 4: Boolean Practice  
+# Create a variable named 'is_raining' set to False.  
+# On the next line, change it to True. 
+# Print "Is it raining?" followed by the boolean. 
+
+is_raining = False
+is_raining = True
+print("Is it raining?", is_raining)
