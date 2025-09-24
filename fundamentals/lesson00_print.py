82fd8ade972134ab773f7338a5a2444c0e0916e0
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-09-24 16:27:32 +0000

    edits

--- fundamentals/lesson00_print.py
+++ fundamentals/lesson00_print.py
@@ -1,4 +1,6 @@
-#KEY CONCEPTS: print statements, running code.
+#KEY CONCEPTS: print statements, comments, running code.
+
+# Write comments after a # sign. Python won't run these lines. They are for your code comments while you develop. It is typical to delete all comments before you deploy your code.
 
 #the print command print() will display its contents in the system terminal. Typically used for debugging your program.  
 
@@ -7,5 +9,5 @@ print("Hello world")
 #print can take in multiple arguments, separated by a comma:
 print("Age: ", 4)
 
-#it can also run expressions and methods (we'll get into that later)
-print(4 + 5)
+#it can also run expressions and methods 
+print("Sum :", 4 + 5)
