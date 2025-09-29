61bd1982d46d2f6f0c3959ba3a18210033361508
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-09-29 08:38:18 -0400

    Update lesson04_libraries.py

--- fundamentals/lesson04_libraries.py
+++ fundamentals/lesson04_libraries.py
@@ -13,7 +13,8 @@ print("Power (2^5): ", math.pow(2, 5))
 print("Pi constant: ", math.pi)
 
 # RANDOM LIBRARY
-
+# Explain pseudorandom numbers. 
+# Watch Tom Scott video on lava lamps and randomness: https://www.youtube.com/watch?v=1cUUfMeOijg
 import random
 print("\n--- Random Library ---")
 
@@ -21,10 +22,20 @@ print("Random integer between 1 and 10: ", random.randint(1, 10))
 print("Random float between 0 and 1: ", random.random())
 print("Random choice from a list: ", random.choice(["red", "blue", "green"]))
 
-# Challenge 1: Physics Problem (Kinetic Energy)
+# Challenge 1: Circle Area
+# Use two variables "radius" and "circle_area" to calculate the area of a circle with a diameter of 14. 
+# The formula to calculate a circle: πr² (Use math.pi for π) (radius = diameter / 2)
 
-# Challenge 2: Circle Area
-# Use the formula πr² to calculate the area of a circle with a diameter of 14. (Use math.pi for π.)
-# Include an expression that calculates the radius based on its diameter (Diameter = r2)
-circle_area = math.pi * (7 ** 2)
-print("Challenge 2 - Circle Area:", circle_area)
+radius = 14 /2
+circle_area = math.pi * (radius ** 2)
+print("Circle Area:", circle_area)
+
+# Challenge 2: Simulate a Die Roll
+# Use the random library to simulate rolling a six-sided die.
+# The output should be a random integer between 1 and 6.
+# Store the result in a variable called "die_roll" and print it.
+
+die_roll = random.randint(1, 6)
+print("Die Roll:", die_roll)
+
+
