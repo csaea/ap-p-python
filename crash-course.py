c15a7d8b2bbf240e2ba2150e0f2ba1ae25719190
Mr. Harrell <183651098+csaea@users.noreply.github.com>
2026-09-08 18:22:24 +0000

    my first commit

---
+++ crash-course.py
@@ -0,0 +1,35 @@
+# comment
+
+# here
+# is
+# a
+# comment
+
+print("Hello world!")
+
+# VARIABLE DECLARATIONS AND DATA TYPES:
+
+a = 4           # integer 
+b = 5.5         # float 
+c = "CSAEA"     # string
+d = False       # boolean 
+
+print(a, b, c, d)
+
+# OPERATORS 
+# + - / *    %  **  //
+# +=   -=   /=   
+
+e = 3 - 1
+print(e)
+e += 20
+print(e)
+
+# f-string
+
+print(f"e is equal to {e}")
+
+e -= 7 
+e += 12
+
+print(f"e is NOW equal to {e}")
